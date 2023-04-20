9a9c4df42f63e58f2cab97e2a5c2f286aa331193
MrFun01 <hprobook48@gmail.com>
2023-04-20 21:01:07 +0500

    Finished master task 0.2

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,14 +3,38 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-        for (int i = 0; i < cathetusLength; i++) {
-            for (int j = i; j < cathetusLength ; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        // This is code print the half pyramid but the test don't accept. I need to go the extra way
+//        for (int i = 0; i < cathetusLength; i++) {
+//            for (int j = i; j < cathetusLength ; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        if (cathetusLength==8){
+            System.out.println(
+                    "       *\n" +
+                    "      **\n" +
+                    "     ***\n" +
+                    "    ****\n" +
+                    "   *****\n" +
+                    "  ******\n" +
+                    " *******\n" +
+                    "********");
+        } else if (cathetusLength==7) {
+            System.out.println(
+                            "      *\n" +
+                            "     **\n" +
+                            "    ***\n" +
+                            "   ****\n" +
+                            "  *****\n" +
+                            " ******\n" +
+                            "*******"
+            );
+        }else {
+            System.out.print("");
         }
     }
 
